b9f7e5ea96a8d0c4e19c1ab81704bf6e2802a3bc
Lucifer-mk <manojkaprimanoj@gmail.com>
2025-01-15 12:38:05 +1100

    Ui Changes

--- script.js
+++ script.js
@@ -7,35 +7,30 @@ let computerScore = 0;
 let yourScore = 0;
 let totalplay = 0;
 let reasult = "";
-
 const winAudio = new Audio("./assets/win.mp3");
 const drawAudio = new Audio ("./assets/draw.mp3");
 const looseAudio = new Audio("./assets/loose.mp3");
-
-
-
 function playAs(playerChoice){
-   const computerChoice = choices[Math.floor(Math.random()*3)];
-    if(computerChoice === playerChoice){
-        reasult = "Its A Draw 🙂‍↔️";
-        totalplay++
-        drawAudio.play();
-    }
-    else if(playerChoice==="rock" && computerChoice==="paper" || playerChoice === "paper" && computerChoice==="scissors" || playerChoice==="scissors" && computerChoice ==="rock" ){
-        reasult = "You Loose😭";
-        computerScore++;
-        totalplay++
-        looseAudio.play();
-
-    }
-    else{
-        reasult = "You Win 😄"
-            yourScore++;
+    const computerChoice = choices[Math.floor(Math.random()*3)];
+        if(computerChoice === playerChoice){
+            reasult = "Its A Draw 🙂‍↔️";
             totalplay++
-            winAudio.play();
+            drawAudio.play();
+        }
+        else if(playerChoice==="rock" && computerChoice==="paper" || playerChoice === "paper" && computerChoice==="scissors" || playerChoice==="scissors" && computerChoice ==="rock" ){
+            reasult = "You Loose😭";
+            computerScore++;
+            totalplay++
+            looseAudio.play();
+        }
+        else{
+            reasult = "You Win 😄"
+                yourScore++;
+                totalplay++
+                winAudio.play();
+        }
+        displayReasult.textContent=`${reasult}`;
+        you.textContent =`Your score: ${yourScore}`
+        computer.textContent =`Computer Score:${computerScore}`
+        timess.textContent =`Total Play: ${totalplay}`
     }
-    displayReasult.textContent=`${reasult}`;
-    you.textContent =`Your score: ${yourScore}`
-    computer.textContent =`Computer Score:${computerScore}`
-    timess.textContent =`totalplay = ${totalplay     }`
-}
